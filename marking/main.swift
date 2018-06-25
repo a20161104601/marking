1ec4b0db7e9fd1304a68684690263f8d729b3827
20161104601 <a20161104601@123.local>
2018-06-25 10:52:37 +0800

    Initial Commit

---
+++ marking/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  marking
+//
+//  Created by 20161104601 on 2018/6/25.
+//  Copyright © 2018年 20161104601. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
